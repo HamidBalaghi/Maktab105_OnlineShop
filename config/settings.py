6bdc622dd5533d34136d121dcf544fc30a0cf744
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-15 19:36:38 +0330

    ⚙ Introducing new Apps

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
+    'core',
+    'customers',
+    'landing',
+    'orders',
+
 ]
 
 MIDDLEWARE = [
@@ -121,3 +128,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+

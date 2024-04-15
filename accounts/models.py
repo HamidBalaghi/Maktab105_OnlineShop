1fdf199881b4e9971ab1725522d3c8b974ed25e8
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-15 19:38:30 +0330

    🧩 Create initial User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractBaseUser
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.CharField(max_length=100, unique=True, validators=[
+        RegexValidator(
+            regex='^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+            message='Enter a valid email address'
+        )
+    ])
+
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', ]
+
+    def __str__(self):
+        return self.name
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin

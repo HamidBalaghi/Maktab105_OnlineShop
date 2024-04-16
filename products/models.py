b562bf11c84926e1a014b4dc349464cb49efb545
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-17 01:09:50 +0330

    🧩 Create initial 'Product', 'Price', 'Image' models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from core.models import TimeStampMixin, LogicalMixin
+from utils.filepath import get_image_upload_path
 
-# Create your models here.
+
+class Product(LogicalMixin, TimeStampMixin):
+    name = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.name} - stock={self.stock}'
+
+
+class Price(LogicalMixin, TimeStampMixin):
+    updated_at = None
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.product.name} - price={self.price}'
+
+
+class Image(LogicalMixin, TimeStampMixin):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(
+        upload_to=get_image_upload_path
+    )
+    def __str__(self):
+        return f'{self.product.brand}-{self.product.name}'

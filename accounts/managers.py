6882a295bf42fb14fed13298b6c1da338f5729d6
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-16 13:43:34 +0330

    🧩 Change 'name' to 'username'

--- accounts/managers.py
+++ accounts/managers.py
@@ -2,18 +2,19 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, name=None, password=None):
+
+    def create_user(self, email, username=None, password=None):
         if not email:
             raise ValueError('User must have a valid email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, username=username)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, name=None, password=None):
-        user = self.create_user(email, name, password)
+    def create_superuser(self, email, username=None, password=None):
+        user = self.create_user(email, username, password)
         user.is_admin = True
         user.is_active = True
         user.save(using=self._db)

cc315e48d140eb6c4a3ea9e247a4576d31de0450
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-16 15:24:21 +0330

    🧩 Change Customer model for logical delete

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models import UniqueConstraint, Q
+from accounts.models import User
+from core.models import TimeStampMixin, LogicalMixin
 
-# Create your models here.
+
+class Customer(LogicalMixin, TimeStampMixin):
+    customer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    phone_number = models.CharField(max_length=11, unique=True, null=True, blank=True)
+    customer_type = models.SmallIntegerField(default=5)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['customer'],
+                condition=Q(is_deleted=False),
+                name='unique_not_deleted_customer'
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.customer.username}"

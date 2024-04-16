c7795649a892740115adef66d9d130137941e882
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-17 02:54:26 +0330

    🧩 Create initial Category model - (M2M with product)

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from core.models import TimeStampMixin, LogicalMixin
 from utils.filepath import get_image_upload_path
 
@@ -8,6 +9,7 @@ class Product(LogicalMixin, TimeStampMixin):
     brand = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     stock = models.PositiveIntegerField()
+    categories = models.ManyToManyField('Category', related_name='products')
 
     def __str__(self):
         return f'{self.name} - stock={self.stock}'
@@ -27,5 +29,26 @@ class Image(LogicalMixin, TimeStampMixin):
     image = models.ImageField(
         upload_to=get_image_upload_path
     )
+
     def __str__(self):
         return f'{self.product.brand}-{self.product.name}'
+
+
+class Category(LogicalMixin, TimeStampMixin):
+    category = models.CharField(max_length=200)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL,
+                               null=True, blank=True,
+                               related_name='child_categories')
+
+    def clean(self):
+        super().clean()
+        if self.parent:
+            if self.parent == self:
+                raise ValidationError("Parent category cannot be itself.")
+            if self.parent.parent == self:
+                raise ValidationError("Child category cannot be the parent of its parent.")
+
+    def __str__(self):
+        if self.parent:
+            return f'{self.category} - parent: {self.parent.category}'
+        return f'{self.category}'

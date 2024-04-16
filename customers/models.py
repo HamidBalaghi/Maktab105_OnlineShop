bad411c7e67cfc051882f66415132a8e77331f6c
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-16 17:35:46 +0330

    🧩 Create Address Model

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.db.models import UniqueConstraint, Q
 from accounts.models import User
@@ -20,3 +21,29 @@ class Customer(LogicalMixin, TimeStampMixin):
 
     def __str__(self):
         return f"{self.customer.username}"
+
+
+class Address(LogicalMixin, TimeStampMixin):
+    updated_at = None
+    is_active = None
+
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    province = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    details = models.TextField()
+    post_code = models.CharField(max_length=10, validators=[
+        RegexValidator(
+            regex='^\d{10}$',
+            message='Code must be exactly 10 digits long'
+        )
+    ])
+    has_paid_order = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.customer} - {self.province} - {self.city}"
+
+    def delete(self):
+        if self.has_paid_order:
+            super().delete()
+        else:
+            super().hard_delete()

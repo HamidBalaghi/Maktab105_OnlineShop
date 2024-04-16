b109222120c2fb3651a9bd8c347486f937c5d577
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-16 17:34:27 +0330

    🧩 Change Queryset and 'archived' property

--- core/managers.py
+++ core/managers.py
@@ -4,8 +4,8 @@ from django.db import models
 class LogicalManager(models.Manager):
 
     def get_queryset(self):
-        return super().get_queryset().filter(is_deleted=False, is_active=True)
+        return super().get_queryset().filter(is_deleted=False)
 
     @property
     def archived(self):
-        return super().get_queryset()
+        return super().get_queryset().filter(is_deleted=True)

c732ba1d0fae9a3267707512f2ce44bc0a0fdf70
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-17 02:55:19 +0330

    🧩 Make initial raw Category Admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from products.models import Product, Image, Price
+from products.models import Product, Image, Price,Category
 
 admin.site.register(Product)
 admin.site.register(Price)
 admin.site.register(Image)
+admin.site.register(Category)

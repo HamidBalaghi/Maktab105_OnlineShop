0cade62f23461ea0959c2fc2dc182448c286a075
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-16 17:36:15 +0330

    🧩 Manage Address Admin

--- customers/admin.py
+++ customers/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, Address
 
 
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('customer', 'phone_number')
 
 
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'province', 'city', 'post_code', 'has_paid_order')
+
+
 admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Address)

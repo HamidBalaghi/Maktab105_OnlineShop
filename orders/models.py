9a815e84d01717d8c87806ee98c04bd1f341a594
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-19 10:37:02 +0330

    🧩 Add Meta class for Order Item

--- orders/models.py
+++ orders/models.py
@@ -3,10 +3,11 @@ from django.db import models
 from core.models import TimeStampMixin, LogicalMixin
 from customers.models import Customer, Address
 # from django.utils import timezone
-# from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError
 from products.models import Product
 from products.models import Discount as ProductDiscount
 from utils.validators import validate_not_in_past
+from django.db.models import UniqueConstraint, Q
 
 
 class Order(LogicalMixin, TimeStampMixin):
@@ -17,6 +18,9 @@ class Order(LogicalMixin, TimeStampMixin):
                                 related_name='orders')
     is_paid = models.BooleanField(default=False)
     paid_time = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.id}-{self.customer}-{self.is_paid}'
 
     # def validate_address_customer_match(self):
     #     if self.address and self.address.customer != self.customer:
@@ -48,8 +52,25 @@ class OrderItem(LogicalMixin, TimeStampMixin):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
     product_discount = models.ForeignKey(ProductDiscount,
-                                         on_delete=models.SET_NULL, null=True,
+                                         on_delete=models.SET_NULL, null=True, blank=True,
                                          related_name='order_items')
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['order', 'product'],
+                condition=Q(is_deleted=False),
+                name='in not deleted order item: unique product and order '
+            )
+        ]
+
+    def clean(self):
+        super().clean()
+        if self.product_discount and self.product_discount.product != self.product:
+            raise ValidationError("Product discount product must be equal to the product.")
+
+    def __str__(self):
+        return f'{self.order} - {self.product} - {self.quantity}'
 
 
 class DiscountCode(LogicalMixin, TimeStampMixin):

6fad0eb7f4272ae9efb7bcb3f8fad63edbe2580b
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-20 19:47:16 +0330

    🎬 Create initial view for 'Signup'

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+from accounts.forms import CustomSignUpForm
+from utils.otp import otp_sender
 
-# Create your views here.
+
+class SignupView(CreateView):
+    form_class = CustomSignUpForm
+    template_name = 'account/signup.html'
+
+    def form_valid(self, form):
+        user = form.save(commit=True)
+        otp_sender(user=user)
+        return render(self.request, 'test/test.html')

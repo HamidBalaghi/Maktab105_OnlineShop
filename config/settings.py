388817033ed57ba2cb186add2a04b1715b4b90f4
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-20 19:52:05 +0330

    ⚙ Add SMTP settings

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,12 @@ AUTH_USER_MODEL = 'accounts.User'
 ##  Media ##
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+## ----EMAIL ----##
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'balaghi.hamid.django@gmail.com'
+EMAIL_HOST_PASSWORD = 'ijht buao obbl sroe'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False

904158211330c2a45933b370b77adbf60e7e4275
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-21 18:40:54 +0330

    🎬 Create initial Login View, and Add OTP code expire Validation

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import CreateView, FormView
-from accounts.forms import CustomSignUpForm, VerifyForm
-from utils.otp import otp_sender
+from accounts.forms import CustomSignUpForm, VerifyForm, CustomUserLoginForm
+from utils.otp import otp_sender, is_otp_expired
 from accounts.models import User, OTPModel
 from customers.models import Customer
 from django.contrib.auth import authenticate, login, logout
+from django.views import View
 
 
 class SignupView(CreateView):
@@ -15,6 +16,29 @@ class SignupView(CreateView):
         user = form.save(commit=True)
         otp_sender(user=user)
         return redirect('accounts:activation', pk=user.pk)
+
+
+class CustomUserLoginView(View):
+    template_name = 'account/login.html'
+
+    def get(self, request, *args, **kwargs):
+        form = CustomUserLoginForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = CustomUserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user and password:
+                login(request, user)
+            elif user:
+                otp_sender(user)
+                return redirect('accounts:activation', pk=user.pk)
+            else:
+                form.add_error(None, 'Invalid email or password')
+        return render(request, self.template_name, {'form': form})
 
 
 class UserActivationView(FormView):
@@ -29,7 +53,9 @@ class UserActivationView(FormView):
 
     def form_valid(self, form):
         code = form.cleaned_data.get('code')
-        if code == str(OTPModel.objects.get(user=self.new_user).code):
+        self.new_user.backend = 'django.contrib.auth.backends.ModelBackend'
+        otp_model = OTPModel.objects.get(user=self.new_user)
+        if code == str(otp_model.code) and not is_otp_expired(otp_model):
             if not self.new_user.is_active:
                 self.new_user.is_active = True
                 self.new_user.save(update_fields=['is_active'])

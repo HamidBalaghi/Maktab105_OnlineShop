118adad0df5a909cc36f09d55f783009c01d91b9
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-21 19:16:22 +0330

    🔄 Add Logout URL

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/', views.SignupView.as_view(), name='signup'),
     path('login/', views.CustomUserLoginView.as_view(), name='login'),
+    path('logout/', views.CustomUserLogoutView.as_view(), name='logout'),
     path('activation/<int:pk>', views.UserActivationView.as_view(), name='activation'),
 ]

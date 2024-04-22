6db4c99d8c62dcc4ef3505a60dfa9d72e774eb73
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-23 01:59:00 +0330

    🎬 Create initial Product Detail View

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from products.models import Product
+from django.shortcuts import get_object_or_404, redirect
 
-# Create your views here.
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product.html'
+
+    # context_object_name = 'product'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.product = get_object_or_404(Product, pk=self.kwargs.get('pk'), slug=self.kwargs.get('slug'))
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'] = self.product.product_details()
+        return context

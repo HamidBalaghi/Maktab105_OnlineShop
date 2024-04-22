26763e1718003d80033b6576c796cf6bd694579a
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-23 01:57:19 +0330

    🔄 Make Product Details URL

---
+++ products/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'products'
+
+
+urlpatterns = [
+    path('products/<slug:slug>/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+]

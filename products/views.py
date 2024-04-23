33724ac8cebb152adf4bed10c38aaae489d372b7
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-24 01:58:02 +0330

    🎬 Create initial View for Products of a Category

--- products/views.py
+++ products/views.py
@@ -30,3 +30,17 @@ class HomePageView(TemplateView):
         for category in categories:
             context['categories'][f"{category.category}"] = category.products.all()
         return context
+
+
+class CategoryProductView(TemplateView):
+    template_name = 'category_products.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.category = get_object_or_404(Category, slug=self.kwargs.get('slug'), pk=self.kwargs.get('pk'))
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = self.category.products.all()
+        context['category'] = f"{self.kwargs.get('slug')}"
+        return context

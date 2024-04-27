23c1ce24077ef61b428217c4becb79920296be95
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-27 15:30:06 +0330

    📧 Create new OTP sender based on Redis

--- utils/otp.py
+++ utils/otp.py
@@ -1,22 +1,14 @@
 from accounts.models import User
 import random
 from django.core.mail import send_mail
-from accounts.models import OTPModel
-from django.utils import timezone
-from datetime import timedelta
-
-
-def is_otp_expired(otp_model: OTPModel):
-    limit_time = timezone.now() - timedelta(minutes=5)
-    return otp_model.updated_at < limit_time
+from django.core.cache import cache
 
 
 def otp_sender(user: User):
     otp = random.randint(100000, 999999)
-    otp_model, created = OTPModel.objects.get_or_create(user=user)
-    if created or (not created and is_otp_expired(otp_model)):
-        otp_model.code = otp
-        otp_model.save()
+    otp_if_not_expired = cache.get(f"{user.email}")
+    if not otp_if_not_expired:
+        cache.set(f"{user.email}", otp, timeout=300)
         subject = 'OTP Verification'
         message = f'Welcome to Our Online Shop\nDear {user.username},\n\nYour OTP is {otp}'
         from_email = 'balaghi.hamid.django@gmail.com'

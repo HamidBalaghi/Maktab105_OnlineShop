4f70e5db8ba2822c957f89465d7f03b3261c1640
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-27 15:11:33 +0330

    ⚙ Add Redis config as Cache

--- config/settings.py
+++ config/settings.py
@@ -144,4 +144,13 @@ EMAIL_HOST_PASSWORD = 'ijht buao obbl sroe'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
-
+## ----CACHE---- ##
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}

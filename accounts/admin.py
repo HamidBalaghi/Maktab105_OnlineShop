7d7bcc8c52d24da4c2dfaafc554892da485f7523
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-29 01:22:23 +0330

    🧩 Add is_staff attribute to User Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,7 @@ from accounts.models import User
 
 
 class UserAccountAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_active', 'is_admin', 'is_deleted')
+    list_display = ('username', 'email', 'is_superuser', 'is_staff', 'is_active', 'is_deleted')
 
 
 admin.site.register(User, UserAccountAdmin)

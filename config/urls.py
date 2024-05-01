b0a71bc6d5e164700d837a7f8bb9a2320f88531c
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-05-02 02:38:49 +0330

    🔄 Include Customer URL

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('', include('products.urls')),
+    path('', include('customers.urls')),
 )
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

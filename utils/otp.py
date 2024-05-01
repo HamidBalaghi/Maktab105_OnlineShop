5d53770fe47f1c334fca3f267538edf9c0a203c1
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-05-01 13:15:21 +0330

    📧 Change inputs of otp_sender for compatibility by Celery

--- utils/otp.py
+++ utils/otp.py
@@ -1,16 +1,15 @@
-from accounts.models import User
 import random
 from django.core.mail import send_mail
 from django.core.cache import cache
 
 
-def otp_sender(user: User):
+def otp_sender(email, username):
     otp = random.randint(100000, 999999)
-    otp_if_not_expired = cache.get(f"{user.email}")
+    otp_if_not_expired = cache.get(f"{email}")
     if not otp_if_not_expired:
-        cache.set(f"{user.email}", otp, timeout=300)
+        cache.set(f"{email}", otp, timeout=300)
         subject = 'OTP Verification'
-        message = f'Welcome to Our Online Shop\nDear {user.username},\n\nYour OTP is {otp}'
+        message = f'Welcome to Our Online Shop\nDear {username},\n\nYour OTP is {otp}'
         from_email = 'balaghi.hamid.django@gmail.com'
-        to_email = user.email
+        to_email = email
         send_mail(subject, message, from_email, [to_email])

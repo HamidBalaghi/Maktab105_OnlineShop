5f27fa7d8b1d750afb14a2563bc852e9db2e6c69
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-05-02 18:29:38 +0330

    💱 Check if User is not AnonymousUser or not

--- core/mixin.py
+++ core/mixin.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.models import AnonymousUser
+
+
 class NavbarMixin:
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
-        context['logged_in_user'] = user
-        context['logged_in_user_pk'] = user.pk
+        if user.is_authenticated and not isinstance(user, AnonymousUser):
+            context['logged_in_user'] = user
+            context['logged_in_user_pk'] = user.pk
         return context

bd4826eab2480cfaeae1636fe654327e0296d4b1
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-05-04 12:26:36 +0330

    👔 Add another Ordering

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,7 +6,7 @@ class UserAccountAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'is_superuser', 'is_staff', 'is_active', 'is_deleted')
     list_filter = ('is_active', 'is_staff', 'is_deleted')
     search_fields = ('username', 'email')
-    ordering = ('is_deleted', 'is_active', 'username', 'created_at')
+    ordering = ('is_deleted', 'is_active', 'is_staff', 'username', 'created_at')
     date_hierarchy = 'created_at'
 
     def get_queryset(self, request):

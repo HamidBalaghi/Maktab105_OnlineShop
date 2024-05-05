44bc4eb6daf5c76047d08f5e1a91d31ce1d38f5f
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-05-06 00:20:59 +0330

    🔄 Make 'Add New Address' URL

--- customers/urls.py
+++ customers/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('edit-profile/', views.EditProfileView.as_view(), name='edit-profile'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('change-password/', views.CustomPasswordChangeView.as_view(), name='change-password'),
+    path('new-address/', views.AddNewAddressView.as_view(), name='new-address'),
 ]

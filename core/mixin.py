c93bfa5347f1d7f8ca3e8b0f6badec11922fdd28
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-05-05 20:10:56 +0330

    💱 Send Current URL to Navbar

--- core/mixin.py
+++ core/mixin.py
@@ -8,4 +8,6 @@ class NavbarMixin:
         if user.is_authenticated and not isinstance(user, AnonymousUser):
             context['logged_in_user'] = user
             context['logged_in_user_pk'] = user.pk
+        else:
+            context['current_url'] = self.request.build_absolute_uri()
         return context

c009bc8dddb4abe62ea93658a7862785107fbbe9
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-05-06 02:42:31 +0330

    🔄 Make Addresses URL

--- customers/urls.py
+++ customers/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('change-password/', views.CustomPasswordChangeView.as_view(), name='change-password'),
     path('new-address/', views.AddNewAddressView.as_view(), name='new-address'),
+    path('address/', views.ShowAddressView.as_view(), name='address'),
 ]

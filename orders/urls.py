0e7bbed901d6e115f5f3c9da2e2cfaab0ece4eec
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-05-11 02:42:54 +0330

    🔄 Add 'checkout' URL

--- orders/urls.py
+++ orders/urls.py
@@ -7,4 +7,5 @@ app_name = 'orders'
 urlpatterns = [
     path('add-product/', views.AddToOrderItem.as_view(), name='add-product'),
     path('cart/', views.CartView.as_view(), name='cart'),
+    path('checkout/', views.CheckoutView.as_view(), name='checkout'),
 ]

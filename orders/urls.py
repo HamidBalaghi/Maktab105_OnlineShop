16ba48081adc4d452fad29ded274b3efaa32ca21
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-05-12 22:57:28 +0330

    🔄 Add Paid Orders URL

--- orders/urls.py
+++ orders/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('add-product/', views.AddToOrderItem.as_view(), name='add-product'),
     path('cart/', views.CartView.as_view(), name='cart'),
     path('checkout/', views.CheckoutView.as_view(), name='checkout'),
+    path('paid-orders/', views.PaidOrdersView.as_view(), name='paid-orders'),
 ]

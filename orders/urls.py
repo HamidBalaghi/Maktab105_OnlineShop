6bf0f0891647fce3a627d8fca109d4eb247b07f2
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-05-13 14:20:28 +0330

    🔄 Add Order Details URL

--- orders/urls.py
+++ orders/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('cart/', views.CartView.as_view(), name='cart'),
     path('checkout/', views.CheckoutView.as_view(), name='checkout'),
     path('paid-orders/', views.PaidOrdersView.as_view(), name='paid-orders'),
+    path('order/<int:pk>/', views.OrderDetailsView.as_view(), name='order-details'),
 ]

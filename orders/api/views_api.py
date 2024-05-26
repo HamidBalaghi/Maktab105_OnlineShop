3703903bedbd24b89b2b30ef6625b6efb57ae244
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-05-27 02:08:45 +0330

    🧭 🎬 Make def Post for Edit Cart

--- orders/api/views_api.py
+++ orders/api/views_api.py
@@ -5,9 +5,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from products.models import Product
-from .serializers import AddToOrderItemSerializer, OrderSerializer
+from .serializers import AddToOrderItemSerializer, OrderSerializer, EditOrderSerializer
 from .mixin import CartInitializerMixinAPI
-from orders.models import Order
+from orders.models import Order, OrderItem
 
 
 class AddToOrderItem(APIView):
@@ -74,3 +74,62 @@ class CartView(CartInitializerMixinAPI, APIView):
 
         serializer = OrderSerializer(order)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = EditOrderSerializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            errors = {}
+            result = {}
+
+            # Check for negative values and collect errors
+            for key, value in data.items():
+                if value is not None and value < 0:
+                    errors[key] = f"{key} must be a non-negative integer."
+                elif key == 'clear_order':
+                    try:
+                        order = Order.objects.get(id=value, is_paid=False)
+                        if order.customer.customer.id == request.user.id:
+                            for item in order.order_items.all():
+                                item.hard_delete()
+                            result[key] = 'Order has been cleared'
+                        else:
+                            raise Order.DoesNotExist
+                    except Order.DoesNotExist:
+                        errors[key] = 'Order not found'
+                else:
+                    try:
+                        order_item = OrderItem.objects.get(id=value)
+                        if order_item.order.customer.customer != request.user:
+                            errors[key] = "Permission denied"
+                        else:
+                            if key == 'delete_item':
+                                order_item.hard_delete()
+                                result[key] = f"{order_item.item_name()} deleted successfully from order"
+                            elif key == 'increase_item':
+                                if order_item.product.stock > order_item.quantity:
+                                    order_item.quantity += 1
+                                    order_item.save()
+                                    result[key] = f"{order_item.item_name()} increased successfully"
+                                else:
+                                    result[key] = f"Not enough stock for {order_item.item_name()}"
+                            elif key == 'decrease_item':
+                                if order_item.quantity > 1:
+                                    order_item.quantity -= 1
+                                    order_item.save()
+                                    result[key] = f"{order_item.item_name()} decreased successfully"
+                                else:
+                                    order_item.hard_delete()
+                                    result[key] = f"{order_item.item_name()} deleted successfully from order"
+                            else:
+                                pass
+                    except OrderItem.DoesNotExist:
+                        errors[key] = "Invalid order item"
+
+            if errors:
+                return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response(result, status=status.HTTP_200_OK)
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

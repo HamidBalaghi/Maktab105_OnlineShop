700a90c08cdb0216478b2b31b8ad3c22f46d187d
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-06-03 00:38:23 +0330

    🧭 🎬 Create Checkout View (API)

--- orders/api/views_api.py
+++ orders/api/views_api.py
@@ -4,8 +4,11 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from orders.models import DiscountCode
+from customers.models import Address, Customer
 from products.models import Product
-from .serializers import AddToOrderItemSerializer, OrderSerializer, EditOrderSerializer
+from .serializers import AddToOrderItemSerializer, OrderSerializer, EditOrderSerializer, CheckoutAddressGETSerializer, \
+    CheckoutPOSTSerializer
 from .mixin import CartInitializerMixinAPI
 from orders.models import Order, OrderItem
 
@@ -133,3 +136,88 @@ class CartView(CartInitializerMixinAPI, APIView):
 
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CheckoutView(CartInitializerMixinAPI, APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, ]
+
+    def get(self, request):
+        try:
+            order = Order.objects.get(customer__customer=request.user, is_paid=False)
+        except Order.DoesNotExist:
+            return Response({'error': 'Order not found'}, status=404)
+
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CheckoutPOSTSerializer(data=request.data)
+        customer = Customer.objects.get(customer=self.request.user)
+        order = Order.objects.get(customer=customer, is_paid=False, is_deleted=False)
+
+        if serializer.is_valid():
+            address_id = serializer.validated_data.get('selected_address')
+            discount_code = serializer.validated_data.get('discount_code')
+            result = {}
+
+            selected_address = Address.objects.get(id=address_id)
+            if selected_address.customer.customer != self.request.user:
+                return Response({'selected_address': 'Invalid customer ID'}, status=400)
+
+            if discount_code:
+                discount = DiscountCode.objects.get(code=discount_code)
+                order_price = order.order_details()['final_order_price']
+                if discount.is_percent_type:
+                    if (order_price - (order_price * (1 - discount.amount / 100))) < discount.max_discount:
+                        final_price_after_discount = order_price * (1 - discount.amount / 100)
+                    else:
+                        final_price_after_discount = order_price - discount.max_discount
+                else:
+                    if order_price - discount.amount < 0:
+                        final_price_after_discount = 0
+                    else:
+                        final_price_after_discount = order_price - discount.amount
+            else:
+                final_price_after_discount = 0
+            result['Price after discount'] = final_price_after_discount
+
+            if serializer.validated_data.get('payment') == True and order.order_items.exists():
+                if not selected_address.has_paid_order:
+                    selected_address.has_paid_order = True
+                    selected_address.save()
+
+                order.address = selected_address
+                order.is_paid = True
+
+                if discount_code:
+                    entered_discount_code = DiscountCode.objects.get(code=discount_code)
+                    order.discount_code = entered_discount_code
+                    entered_discount_code.is_used = True  # expire discount code after use
+                    entered_discount_code.save()
+
+                order.save()
+
+                for item in order.order_items.filter(is_deleted=False):
+                    item.product_discount = item.product.discounts.first()
+                    item.price = item.product.prices.first()  # Saving related price of product, at the time of purchase
+
+                    product = item.product
+                    product.stock -= item.quantity  # Decrease stock of product after purchase
+
+                    item.save()
+                    product.save()
+                Order.objects.create(customer=customer)  # Create a new Order after Payment
+
+                result['Entered DiscountCode'] = discount_code
+                result['Discount of code'] = order.order_details()['final_order_price'] - final_price_after_discount
+                result['Selected Address'] = {
+                    'province': selected_address.province,
+                    'city': selected_address.city,
+                    'details': selected_address.details,
+                    'postal_code': selected_address.post_code
+                }
+
+            return Response(result, status=200)
+        else:
+            return Response(serializer.errors, status=400)
